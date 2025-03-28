dd65f7329c96e5cc1003d6e305f84b023acf69f8
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2025-03-28 17:40:27 +0700

    5654651651

--- controllers/external/docexlog.js
+++ controllers/external/docexlog.js
@@ -340,50 +340,104 @@ exports.person = async (req, res) => {
     const docstatus = Number(docstatusId);
     let persons = [];
 
-    if (docstatus === 5 || docstatus === 7) {
-      persons = await prisma.docexLog.findMany({
-        where: {
-          docexId: Number(docexId),
-          receiverCode: req.user.emp_code,
-          OR: [{ docstatusId: 2 }, { docstatusId: 6 }],
-        },
-        orderBy: {
-          id: "desc",
-        },
-        take: 1,
-        include: {
-          assigner: {
-            select: {
-              gender: true,
-              first_name: true,
-              last_name: true,
-              emp_code: true,
+    const docex = await prisma.docExternal.findUnique({
+      where: {
+        id: Number(docexId),
+      },
+    });
+
+    if (docex.extype === 1) {
+      if (docstatus === 5 || docstatus === 7) {
+        persons = await prisma.docexLog.findMany({
+          where: {
+            docexId: Number(docexId),
+            receiverCode: req.user.emp_code,
+            OR: [{ docstatusId: 2 }, { docstatusId: 6 }],
+          },
+          orderBy: {
+            id: "desc",
+          },
+          take: 1,
+          include: {
+            assigner: {
+              select: {
+                gender: true,
+                first_name: true,
+                last_name: true,
+                emp_code: true,
+              },
             },
           },
-        },
-      });
-    } else if (docstatus === 6 || docstatus === 3) {
-      persons = await prisma.docexLog.findMany({
-        where: {
-          docexId: Number(docexId),
-          receiverCode: req.user.emp_code,
-          OR: [{ docstatusId: 5 }, { docstatusId: 7 }],
-        },
-        orderBy: {
-          id: "desc",
-        },
-        take: 1,
-        include: {
-          assigner: {
-            select: {
-              gender: true,
-              first_name: true,
-              last_name: true,
-              emp_code: true,
+        });
+      } else if (docstatus === 6 || docstatus === 3) {
+        persons = await prisma.docexLog.findMany({
+          where: {
+            docexId: Number(docexId),
+            receiverCode: req.user.emp_code,
+            OR: [{ docstatusId: 5 }, { docstatusId: 7 }],
+          },
+          orderBy: {
+            id: "desc",
+          },
+          take: 1,
+          include: {
+            assigner: {
+              select: {
+                gender: true,
+                first_name: true,
+                last_name: true,
+                emp_code: true,
+              },
             },
           },
-        },
-      });
+        });
+      }
+    } else if (docex.extype === 2) {
+      if (docstatus === 1 || docstatus === 2 || docstatus === 10) {
+        persons = await prisma.docexLog.findMany({
+          where: {
+            docexId: Number(docexId),
+            receiverCode: req.user.emp_code,
+            OR: [{ docstatusId: 3 }],
+          },
+          orderBy: {
+            id: "desc",
+          },
+          take: 1,
+          include: {
+            assigner: {
+              select: {
+                gender: true,
+                first_name: true,
+                last_name: true,
+                emp_code: true,
+              },
+            },
+          },
+        });
+      } else if (docstatus === 3) {
+        persons = await prisma.docexLog.findMany({
+          where: {
+            docexId: Number(docexId),
+            receiverCode: req.user.emp_code,
+            OR: [{ docstatusId: 1 }, { docstatusId: 2 }, { docstatusId: 10 }],
+          },
+          orderBy: {
+            id: "desc",
+          },
+          take: 1,
+          include: {
+            assigner: {
+              select: {
+                gender: true,
+                first_name: true,
+                last_name: true,
+                emp_code: true,
+              },
+            },
+          },
+        });
+      }
     }
 
     res.json(persons);
@@ -513,7 +567,7 @@ exports.gethistoryall = async (req, res) => {
       orderBy: [
         { receiver: { department: { id: "desc" } } },
         { createdAt: "asc" },
-        // { departmentactive: "asc" } 
+        // { departmentactive: "asc" }
       ],
       include: {
         docstatus: true,
