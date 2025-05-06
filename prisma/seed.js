68c04935d9f9aa8d0c66b90f1a8b26e7134b1367
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2025-05-06 10:16:49 +0700

    165465512

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,22 +1,22 @@
-// const seedDepartment = require("./seedDepartment");
-// const seedDivision = require("./seedDivision");
-// const seedOffice = require("./seedOffice");
-// const seedUnit = require("./seedUnit");
-// const seedPositionGroup = require("./seedPositionGroup");
-// const seedPositionCode = require("./seedPositionCode");
-// const seedPosition = require("./seedPosition");
-// const seedEmployee = require("./seedEmployee");
+const seedDepartment = require("./seedDepartment");
+const seedDivision = require("./seedDivision");
+const seedOffice = require("./seedOffice");
+const seedUnit = require("./seedUnit");
+const seedPositionGroup = require("./seedPositionGroup");
+const seedPositionCode = require("./seedPositionCode");
+const seedPosition = require("./seedPosition");
+const seedEmployee = require("./seedEmployee");
 const seedUser = require("./seedUser");
 
 async function main() {
-  // await seedDepartment(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Department
-  // await seedDivision(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Division
-  // await seedOffice(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Office
-  // await seedUnit(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Unit
-  // await seedPositionGroup(); // เรียกใช้ฟังก์ชัน seeding สำหรับ PositionGroup
-  // await seedPositionCode(); // เรียกใช้ฟังก์ชัน seeding สำหรับ PositionCode
-  // await seedPosition(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Position
-  // await seedEmployee(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Employee
+  await seedDepartment(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Department
+  await seedDivision(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Division
+  await seedOffice(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Office
+  await seedUnit(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Unit
+  await seedPositionGroup(); // เรียกใช้ฟังก์ชัน seeding สำหรับ PositionGroup
+  await seedPositionCode(); // เรียกใช้ฟังก์ชัน seeding สำหรับ PositionCode
+  await seedPosition(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Position
+  await seedEmployee(); // เรียกใช้ฟังก์ชัน seeding สำหรับ Employee
   await seedUser(); // เรียกใช้ฟังก์ชัน seeding สำหรับ User
 
   console.log("Seeding complete!");
