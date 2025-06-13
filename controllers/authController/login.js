10b520185705db15d999ff76a31a5b7509e2bc44
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2025-06-13 16:51:46 +0700

    5456165165

--- controllers/authController/login.js
+++ controllers/authController/login.js
@@ -34,83 +34,89 @@ module.exports = async (req, res) => {
 
     const token = await loginAndGetToken();
 
-    const response = await axios.get(
-      `${process.env.URL_API}/organization-svc/employee/get?search=${username}`,
-      {
-        headers: { Authorization: `Bearer ${token}` },
+    if (token) {
+      try {
+        const response = await axios.get(
+          `${process.env.URL_API}/organization-svc/employee/get?search=${username}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        const usersData = response.data.data.employees;
+
+        if (usersData.length > 0) {
+          const userData = usersData[0];
+
+          await prisma.employee.upsert({
+            where: { emp_code: userData.emp_code },
+            update: {
+              first_name: userData.first_name_la,
+              last_name: userData.last_name_la,
+              emp_code: userData.emp_code,
+              status: userData.status,
+              gender: userData.gender,
+              posId: Number(userData.office?.pos_id) || null,
+              departmentId: Number(userData.office?.department_id) || null,
+              divisionId: Number(userData.office?.division_id) || null,
+              officeId: Number(userData.office?.office_id) || null,
+              unitId:
+                userData.office.unit_id === 0 ||
+                userData.office.unit_id === null
+                  ? null
+                  : userData.office?.unit_id,
+              tel: userData.phone || null,
+              email: userData.email || null,
+              empimg: userData.image
+                ? `https://uat-api.edl.com.la/api_v2/organization-svc/employee/getEmpImg/${userData.emp_code}/${userData.image}`
+                : null,
+              createdAt: userData.created_at
+                ? new Date(userData.created_at)
+                : new Date(),
+              updatedAt: userData.created_at
+                ? new Date(userData.created_at)
+                : new Date(),
+            },
+            create: {
+              id: userData.emp_id,
+              first_name: userData.first_name_la,
+              last_name: userData.last_name_la,
+              emp_code: userData.emp_code,
+              status: userData.status,
+              gender: userData.gender,
+              posId: Number(userData.office?.pos_id) || null,
+              departmentId: Number(userData.office?.department_id) || null,
+              divisionId: Number(userData.office?.division_id) || null,
+              officeId: Number(userData.office?.office_id) || null,
+              unitId:
+                userData.office.unit_id === 0 ||
+                userData.office.unit_id === null
+                  ? null
+                  : userData.office?.unit_id,
+              tel: userData.phone || null,
+              email: userData.email || null,
+              empimg: userData.image
+                ? `https://uat-api.edl.com.la/api_v2/organization-svc/employee/getEmpImg/${userData.emp_code}/${userData.image}`
+                : null,
+              createdAt: userData.created_at
+                ? new Date(userData.created_at)
+                : new Date(),
+              updatedAt: userData.created_at
+                ? new Date(userData.created_at)
+                : new Date(),
+            },
+          });
+
+          await prisma.user.update({
+            where: { username: userData.emp_code },
+            data: {
+              employeeId: userData.emp_id,
+            },
+          });
+        }
+      } catch (err) {
+        console.warn("Failed to fetch employee info. Continuing without sync.");
       }
-    );
-
-    const usersData = response.data.data.employees;
-
-    if (usersData.length > 0) {
-      const userData = usersData[0];
-
-      await prisma.employee.upsert({
-        where: { emp_code: userData.emp_code },
-        update: {
-          first_name: userData.first_name_la,
-          last_name: userData.last_name_la,
-          emp_code: userData.emp_code,
-          status: userData.status,
-          gender: userData.gender,
-          posId: Number(userData.office?.pos_id) || null,
-          departmentId: Number(userData.office?.department_id) || null,
-          divisionId: Number(userData.office?.division_id) || null,
-          officeId: Number(userData.office?.office_id) || null,
-          unitId:
-            userData.office.unit_id === 0 || userData.office.unit_id === null
-              ? null
-              : userData.office?.unit_id,
-          tel: userData.phone || null,
-          email: userData.email || null,
-          empimg: userData.image
-            ? `https://uat-api.edl.com.la/api_v2/organization-svc/employee/getEmpImg/${userData.emp_code}/${userData.image}`
-            : null,
-          createdAt: userData.created_at
-            ? new Date(userData.created_at)
-            : new Date(),
-          updatedAt: userData.created_at
-            ? new Date(userData.created_at)
-            : new Date(),
-        },
-        create: {
-          id: userData.emp_id,
-          first_name: userData.first_name_la,
-          last_name: userData.last_name_la,
-          emp_code: userData.emp_code,
-          status: userData.status,
-          gender: userData.gender,
-          posId: Number(userData.office?.pos_id) || null,
-          departmentId: Number(userData.office?.department_id) || null,
-          divisionId: Number(userData.office?.division_id) || null,
-          officeId: Number(userData.office?.office_id) || null,
-          unitId:
-            userData.office.unit_id === 0 || userData.office.unit_id === null
-              ? null
-              : userData.office?.unit_id,
-          tel: userData.phone || null,
-          email: userData.email || null,
-          empimg: userData.image
-            ? `https://uat-api.edl.com.la/api_v2/organization-svc/employee/getEmpImg/${userData.emp_code}/${userData.image}`
-            : null,
-          createdAt: userData.created_at
-            ? new Date(userData.created_at)
-            : new Date(),
-          updatedAt: userData.created_at
-            ? new Date(userData.created_at)
-            : new Date(),
-        },
-      });
-
-      await prisma.user.update({
-        where: { username: userData.emp_code },
-        data: {
-          employeeId: userData.emp_id,
-        },
-      });
-
-      // console.log(`Synchronized user: ${userData.first_name_la}`);
     }
     // Step 1 Check Email in DB
     const user = await prisma.user.findUnique({
